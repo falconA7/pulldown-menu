bd0ef61a34b65688f0173ef54f5a6d39ffbb8610
shunta aoki <kaweco17@gamil.com>
2026-03-02 22:26:32 +0900

    code exchange

--- pulldown-menu/script.js
+++ pulldown-menu/script.js
@@ -1,3 +1,22 @@
+const lang = document.querySelector('html').lang;
+
+var opt;
+switch(lang){
+    case 'ja':
+        opt = document.querySelector('option[value="index.html"]');
+        break;
+    case 'en':
+        opt = document.querySelector('option[value="index-en.html"]');
+        break;
+    case 'zh':
+        opt = document.querySelector('option[value="index-zh.html"]');
+        break;
+}
+opt.selected = true;
+
 document.getElementById('form').select.onchange = function(){
     location.href = document.getElementById('form').select.value
 }
+
+
+
