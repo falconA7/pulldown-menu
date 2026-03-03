7d594e18268f66e9ae6a9383185306695a41b96f
shunta aoki <kaweco17@gamil.com>
2026-03-03 12:34:16 +0900

    最小の変更

--- pulldown-menu/script.js
+++ pulldown-menu/script.js
@@ -1,3 +1,19 @@
+const lang = document.querySelector('html').lang;
+
+let opt;
+switch(lang){
+    case 'ja':
+        opt = document.querySelector('option[value="index.html"]');
+        break;
+    case 'en':
+        opt = document.querySelector('option[value="index-en.html"]');
+        break;
+    case 'zh':
+        opt = document.querySelector('option=[value="index-zh.html"]');
+        break;
+
+}
+
 document.getElementById('form').select.onchange = function(){
     location.href = document.getElementById('form').select.value
 }
